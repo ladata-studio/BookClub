baf6509068dcf69d815350a31e85125dc2ce6731
Ladata Studio <ladata.studio@gmail.com>
2022-12-23 15:22:43 +0100

    Scrolling Bug Fix

--- src/components/BooksContainer/index.jsx
+++ src/components/BooksContainer/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Book from '../Book'
+import {debounce} from 'lodash-es'
 import {BookList, Container, H2} from './styles'
 
 const BooksContainer = ({books, onSelect, isPanelOpen}) => {
+  const [scroll, setScroll] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      setScroll(window.scrollY)
+    }, 100)
+
+    if (!isPanelOpen) {
+      window.addEventListener('scroll', handleScroll)
+    }
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [isPanelOpen])
+
+  useEffect(() => {
+    if (!isPanelOpen) {
+      window.scroll(0, scroll)
+      console.log('scrollingggg')
+    }
+  }, [isPanelOpen, scroll])
+
   return (
-    <Container isPanelOpen={isPanelOpen}>
+    <Container isPanelOpen={isPanelOpen} top={scroll}>
       <H2>All books</H2>
       <BookList>
         {books.map((book) => (
